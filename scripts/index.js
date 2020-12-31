4986fd0e083c4a1d4906b256f36b1791d9084424
Zenoix <jeff.wang0296@gmail.com>
2021-01-01 02:16:12 +1300

    Finished dark theme for now

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,13 @@
 const btn = document.querySelector(".btn-toggle");
+const img = document.getElementById("banner");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
 btn.addEventListener("click", function () {
-  if (prefersDarkScheme.matches) {
-    document.body.classList.toggle("light-theme");
-  } else {
-    document.body.classList.toggle("dark-theme");
-  }
+    if (prefersDarkScheme.matches) {
+        document.body.classList.toggle("light-theme");
+        img.classList.toggle("light-theme");
+    } else {
+        document.body.classList.toggle("dark-theme");
+        img.classList.toggle("dark-theme");
+    }
 });
