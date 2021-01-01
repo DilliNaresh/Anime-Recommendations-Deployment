9e09cda4978571cb28dc90767bfd3ad3883572f9
Zenoix <jeff.wang0296@gmail.com>
2021-01-01 20:39:51 +1300

    Continued working on dark theme, now stores user preference and uses icons to select

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,30 @@
 const btn = document.querySelector(".btn-toggle");
 const img = document.getElementById("banner");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+var theme = null;
+if (localStorage.getItem("theme") == null) {
+    if (prefersDarkScheme.matches) {
+        theme = "dark";
+    }
+    else {
+        theme = "light";
+    }
+    localStorage.setItem("theme", theme);
+}
+theme = localStorage.getItem("theme")
+
+const currentTheme = localStorage.getItem("theme");
+
+if (prefersDarkScheme.matches && currentTheme === "light") {
+    document.body.classList.toggle("light-theme");
+    img.classList.toggle("light-theme");
+    document.getElementById("themeicon").src="images/light-mode-icon.png"
+} else if (!prefersDarkScheme.matches && currentTheme === "dark"){
+    document.body.classList.toggle("dark-theme");
+    img.classList.toggle("dark-theme");
+}
+
+console.log(theme)
 
 btn.addEventListener("click", function () {
     if (prefersDarkScheme.matches) {
@@ -10,4 +34,14 @@ btn.addEventListener("click", function () {
         document.body.classList.toggle("dark-theme");
         img.classList.toggle("dark-theme");
     }
+    if (theme === "dark") {
+        theme = "light";
+        document.getElementById("themeicon").src="images/light-mode-icon.png"
+    }
+    else {
+        theme = "dark";
+        document.getElementById("themeicon").src="images/dark-mode-icon.png"
+    }
+    localStorage.setItem("theme", theme);
+    console.log(theme)
 });
