e7e71dd6d13c941d949ec329abc3c7bc5ec27718
Zenoix <jeff.wang0296@gmail.com>
2021-01-01 20:43:20 +1300

    Added alert in case user clicks on next button and formatted code

--- scripts/index.js
+++ scripts/index.js
@@ -5,26 +5,25 @@ var theme = null;
 if (localStorage.getItem("theme") == null) {
     if (prefersDarkScheme.matches) {
         theme = "dark";
-    }
-    else {
+    } else {
         theme = "light";
     }
     localStorage.setItem("theme", theme);
 }
-theme = localStorage.getItem("theme")
+theme = localStorage.getItem("theme");
 
 const currentTheme = localStorage.getItem("theme");
 
 if (prefersDarkScheme.matches && currentTheme === "light") {
     document.body.classList.toggle("light-theme");
     img.classList.toggle("light-theme");
-    document.getElementById("themeicon").src="images/light-mode-icon.png"
-} else if (!prefersDarkScheme.matches && currentTheme === "dark"){
+    document.getElementById("themeicon").src = "images/light-mode-icon.png";
+} else if (!prefersDarkScheme.matches && currentTheme === "dark") {
     document.body.classList.toggle("dark-theme");
     img.classList.toggle("dark-theme");
 }
 
-console.log(theme)
+console.log(theme);
 
 btn.addEventListener("click", function () {
     if (prefersDarkScheme.matches) {
@@ -36,12 +35,15 @@ btn.addEventListener("click", function () {
     }
     if (theme === "dark") {
         theme = "light";
-        document.getElementById("themeicon").src="images/light-mode-icon.png"
-    }
-    else {
+        document.getElementById("themeicon").src = "images/light-mode-icon.png";
+    } else {
         theme = "dark";
-        document.getElementById("themeicon").src="images/dark-mode-icon.png"
+        document.getElementById("themeicon").src = "images/dark-mode-icon.png";
     }
     localStorage.setItem("theme", theme);
-    console.log(theme)
+    console.log(theme);
 });
+
+function nextButtonAlert() {
+    alert("This section is still under progress");
+}
