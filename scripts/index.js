922583edc664e771f723359789a003ba215eedde
Zenoix <jeff.wang0296@gmail.com>
2021-01-05 02:04:25 +1300

    Cleaned up some of the if else statements

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,12 @@ const btn = document.querySelector(".btn-toggle");
 const img = document.getElementById("banner");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 var theme = null;
+
 if (localStorage.getItem("theme") == null) {
-    if (prefersDarkScheme.matches) {
-        theme = "dark";
-    } else {
-        theme = "light";
-    }
+    theme = prefersDarkScheme.matches ? "dark" : "light";
     localStorage.setItem("theme", theme);
 }
+
 theme = localStorage.getItem("theme");
 
 const currentTheme = localStorage.getItem("theme");
@@ -31,13 +29,8 @@ btn.addEventListener("click", function () {
         document.body.classList.toggle("dark-theme");
         img.classList.toggle("dark-theme");
     }
-    if (theme === "dark") {
-        theme = "light";
-        document.getElementById("themeicon").src = "images/light-mode-icon.png";
-    } else {
-        theme = "dark";
-        document.getElementById("themeicon").src = "images/dark-mode-icon.png";
-    }
+    theme = theme === "dark" ? "light" : "dark";
+    document.getElementById("themeicon").src = `images/${theme}-mode-icon.png`;
     localStorage.setItem("theme", theme);
 });
 
